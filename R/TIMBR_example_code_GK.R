f7b751f8c792e5ed0b9e04cfd4a0f9648264703a
fboehm <frederick.boehm@gmail.com>
2019-10-24 09:32:48 -0400

    fixed gk example

--- R/TIMBR_example_code_GK.R
+++ R/TIMBR_example_code_GK.R
@@ -2,7 +2,6 @@
 ## - TIMBR can't currently include a kinship matrix
 ##   It's a Bayesian procedure and adding an additional
 ##   variance component with a dense covariance matrix
-<<<<<<< HEAD
 ##   is challenging, if not infeasible computationally. 
 ##   My experience is that the CC and DO don't really need 
 ##   the kinship adjustment a majority of the time.
@@ -11,16 +10,6 @@
 ##   potentially a strong assumption. Nevertheless, here
 ##   I go along with that assumption and put zeros in at 
 ##   all the heterozygous states. It is particularly 
-=======
-##   is challenging, if not infeasible computationally.
-##   My experience is that the CC and DO don't really need
-##   the kinship adjustment a majority of the time.
-## - TIMBR needs the full genoprobs (36 state). For the
-##   CC, qtl2 forces the genoprobs to be homozygous,
-##   potentially a strong assumption. Nevertheless, here
-##   I go along with that assumption and put zeros in at
-##   all the heterozygous states. It is particularly
->>>>>>> b50dac7b2bdef54879a4b3e3b38fa2d047ff27d7
 ##   important for the DO. If you generate the qtl2 data, that
 ##   shouldn't be a problem, but it is a major pain at JAX
 ##   because everyone tends to collapse to additive dosages
@@ -37,13 +26,8 @@ data(mcv.data)
 # Suggested by Wes
 # Influences how much prior weight it places on more or less complicated allelic series
 prior_M <- list(model.type = "crp", # crp - Chinese Restaurant Process
-<<<<<<< HEAD
-                prior.alpha.type = "gamma", 
-                prior.alpha.shape = 1, 
-=======
                 prior.alpha.type = "gamma",
                 prior.alpha.shape = 1,
->>>>>>> b50dac7b2bdef54879a4b3e3b38fa2d047ff27d7
                 prior.alpha.rate = 2.333415)
 
 ## Grab QTL probability matrix
@@ -51,7 +35,7 @@ prior_M <- list(model.type = "crp", # crp - Chinese Restaurant Process
 # qtl_locus - name of QTL locus, needed to pull it from the genoprobs
 n <- nrow(cc_genoprobs[[qtl_chr]][,, qtl_locus])
 qtl_full_genoprobs <- cbind(cc_genoprobs[[qtl_chr]][,, qtl_locus],
-                            matrix(0, ncol = 28, nrow = n)
+                            matrix(0, ncol = 28, nrow = n))
 
 prior_d <- list(P = qtl_full_genoprobs,
                 A = mcv.data$prior.D$A, # Describes the mapping from full genoprobs to additive dosages
@@ -59,11 +43,7 @@ prior_d <- list(P = qtl_full_genoprobs,
 #call TIMBR
 results <- TIMBR(y = cc_phenotype[rownames(qtl_full_genoprobs)], # Order needs to match genoprobs
                  Z = cbind(rep(1, n), cc_addcovar),
-<<<<<<< HEAD
-                 prior.D = prior_d, 
-=======
                  prior.D = prior_d,
->>>>>>> b50dac7b2bdef54879a4b3e3b38fa2d047ff27d7
                  prior.M = prior_M)
 #expected number of alleles
 mean(results$post.K)
