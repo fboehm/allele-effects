e107479616b66c93dea4ff690bd7edc0de9ff407
fboehm <frederick.boehm@gmail.com>
2019-11-04 18:44:27 -0500

    purled a .R file from the .Rmd

--- Rscript/cluster-TIMBR.R
+++ Rscript/cluster-TIMBR.R
@@ -1,10 +1,3 @@
-install.packages(c("devtools", "tidyverse"))
-
-devtools::install_github("fboehm/qtl2effects")
-
-library(qtl2effects)
-library(tidyverse)
-
 rmarkdown::render("../Rmd/tnseq-timbr.Rmd") # contains call to parallel::mclapply
 
 
---
+++ Rscript/tnseq-timbr.R
@@ -0,0 +1,79 @@
+## ------------------------------------------------------------------------
+library(tidyverse)
+library(TIMBR)
+
+
+## ----read-csv------------------------------------------------------------
+(tt <- readr::read_csv("../data/neto_traits_by_probe3_annotated.csv"))
+neto <- tt %>%
+  tidyr::pivot_longer(cols = V2:V35, values_to = "trait", names_to = "trait_name") %>%
+  dplyr::filter(!is.na(trait)) %>%
+  dplyr::select(- trait_name, - neto.n, - row, - n.traits, - cM, -hs, - chr)
+
+
+## ----load-genotypes------------------------------------------------------
+fns <- dir("../data/36-state-genotypes")
+geno <- list()
+for (i in seq_along(fns)){
+  load(file.path("../data/36-state-genotypes", fns[i]))
+  geno[[i]] <- prsmth
+}
+names(geno) <- stringr::str_split_fixed(fns, ".genotype.probs.Rdata", 2)[, 1]
+
+
+## ----prior_M-define------------------------------------------------------
+##### From GK example code
+# Specify allelic series prior
+# Suggested by Wes
+# Influences how much prior weight it places on more or less complicated allelic series
+prior_M <- list(model.type = "crp", # crp - Chinese Restaurant Process
+                prior.alpha.type = "gamma",
+                prior.alpha.shape = 1,
+                prior.alpha.rate = 2.333415)
+
+
+## ----load-hot------------------------------------------------------------
+if (!file.exists("../data/tnseq-traits.rds")) {
+  load("../data/combined_hotspot_traits.Rdata")
+  saveRDS(pheno, "../data/tnseq-traits.rds")
+  pheno -> tnseq_traits
+} else {
+  tnseq_traits <- readRDS("../data/tnseq-traits.rds")
+}
+
+
+## ----load-covariates-----------------------------------------------------
+load("../data/reduced_map_qtl2_mapping_objects.Rdata")
+
+
+## ----load-data-timbr-----------------------------------------------------
+data(mcv.data) # get A matrix
+
+
+## ----make_neto_list------------------------------------------------------
+neto_list <- apply(FUN = as.list, X = neto, MARGIN = 1)
+neto_small <- neto_list[1:3]
+
+
+## ------------------------------------------------------------------------
+outfn <- "../data/timbr-tnseq-results.rds"
+# ensure that inputs to call_timbr all have subjects in same order!
+subject_ids <- rownames(tnseq_traits)
+indices_addcovar <- match(subject_ids, rownames(addcovar))
+addcovar <- addcovar[indices_addcovar, ]
+##
+if (!file.exists(outfn)){
+  timbr_out <- parallel::mclapply(neto_small, 
+                                  FUN = qtl2effects::call_timbr, 
+                                  mc.cores = parallel::detectCores(),
+                                  traits_df = tnseq_traits,
+                                  prior_M = prior_M, 
+                                  genoprobs = geno
+                                  )
+  save(timbr_out, outfn)
+}
+
+
+## ----read_timbr_results--------------------------------------------------
+timbr_out <- readRDS(outfn)
+
