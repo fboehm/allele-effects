212844df66e207e33d97e78f0ad72cb7739c50d5
fboehm <frederick.boehm@gmail.com>
2019-11-04 18:52:38 -0500

    updated filename

--- shell/bsub-mac.sh
+++ shell/bsub-mac.sh
@@ -7,4 +7,4 @@
 #BSUB -n 64 
 #BSUB -R 'span[hosts=1]'
 
-module load R/3.5.1_gcc8.1.0 && module load gcc/8.1.0 && Rscript --vanilla ../Rscript/cluster-TIMBR.R
+module load R/3.5.1_gcc8.1.0 && module load gcc/8.1.0 && Rscript --vanilla ../Rscript/tnseq-timbr.R
