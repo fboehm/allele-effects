dd496643034c762460494d9822a9ac164e068c7f
fboehm <frederick.boehm@gmail.com>
2019-11-05 14:42:28 -0500

    fixed shell script

--- shell/bsub-mac.sh
+++ shell/bsub-mac.sh
@@ -3,8 +3,9 @@
 #BSUB -q long
 #BSUB -o %J.out
 #BSUB -e %J.err
-#BSUB -W 10:00
+#BSUB -W 20:00
+#BSUB -R 'rusage[mem=2048]'
 #BSUB -n 64 
 #BSUB -R 'span[hosts=1]'
 
-module load R/3.5.1_gcc8.1.0 && module load gcc/8.1.0 && Rscript --vanilla ../Rscript/tnseq-timbr.R
+module load R/3.5.1_gcc8.1.0 && module load gcc/8.1.0 && module load pandoc/2.7.2 && Rscript --vanilla ../Rscript/tnseq-timbr.R
