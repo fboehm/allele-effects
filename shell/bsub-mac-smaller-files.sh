c641bfab852f894de47e5dc329858f46481a1878
fboehm <frederick.boehm@gmail.com>
2019-11-12 10:06:58 -0500

    added shell file for making small files

---
+++ shell/bsub-mac-smaller-files.sh
@@ -0,0 +1,11 @@
+### WRITTEN ON MACBOOKPRO
+
+#BSUB -q long
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -W 20:00
+#BSUB -R 'rusage[mem=2048]'
+#BSUB -n 64 
+#BSUB -R 'span[hosts=1]'
+
+module load R/3.5.1_gcc8.1.0 && module load gcc/8.1.0 && module load pandoc/2.7.2 && Rscript --vanilla -e 'rmarkdown::render("../Rscript/timbr-small-files.Rmd")'
