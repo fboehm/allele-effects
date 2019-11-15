60da1033d5709548d9db28077237f5da30a196f2
fboehm <frederick.boehm@gmail.com>
2019-11-15 10:12:01 -0500

    fixed shell script

--- shell/bsub-mac.sh
+++ shell/bsub-mac.sh
@@ -8,4 +8,4 @@
 #BSUB -n 64 
 #BSUB -R 'span[hosts=1]'
 
-module load R/3.5.1_gcc8.1.0 && module load gcc/8.1.0 && module load pandoc/2.7.2 && Rscript --vanilla -e 'rmarkdown::render("../Rscript/tnseq-timbr.Rmd")'
+module load R/3.5.1_gcc8.1.0 && module load gcc/8.1.0 && module load pandoc/2.7.2 && Rscript --vanilla -e 'rmarkdown::render("../Rmd/tnseq-timbr-hotspots.Rmd")'
