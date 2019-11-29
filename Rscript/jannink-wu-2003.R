db412ca95719687eefdb8987b0b026ea1f966946
fboehm <frederick.boehm@gmail.com>
2019-11-29 15:47:57 -0500

    added R file for Jannink & Wu

---
+++ Rscript/jannink-wu-2003.R
@@ -0,0 +1,11 @@
+# Jannink and Wu 2003
+
+
+# update QTL inheritance matrix Q
+
+
+
+# update QTL allelic effects a
+# update family means b and residual variance s2
+# update QTL allelic configuration conditional on allelic number, C| l;
+# update the number of alleles l
